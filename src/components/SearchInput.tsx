053476d8e2b30aa5d111f1426cde99b49a97710e
AdoraWyne <dec_kwynez@hotmail.com>
2026-04-29 16:25:01 +1000

    Build basic search input component with basic style

--- src/components/SearchInput.tsx
+++ src/components/SearchInput.tsx
@@ -1,3 +1,18 @@
-const SearchInput: React.FC = () => {};
+const SearchInput: React.FC = () => {
+  return (
+    <form>
+      <input
+        className="border border-gray-300 rounded-sm p-2 w-full bg-pink-50 my-4"
+        type="text"
+        id="search-query"
+        name="search-query"
+      />
+      <br />
+      <button className="border border-gray-300 rounded-sm p-2" type="submit">
+        Submit
+      </button>
+    </form>
+  );
+};
 
 export default SearchInput;
