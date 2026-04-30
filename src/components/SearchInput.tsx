6c5db06380fd9e150e943d939958ab35166b6da4
AdoraWyne <dec_kwynez@hotmail.com>
2026-04-30 15:35:32 +1000

    - Implement Tanstack query - test with a hardcoded API call to make sure useQuery hook up correctly

--- src/components/SearchInput.tsx
+++ src/components/SearchInput.tsx
@@ -1,7 +1,23 @@
 import { useState } from "react";
+import { useQuery } from "@tanstack/react-query";
 
 const SearchInput: React.FC = () => {
   const [value, setValue] = useState<string>("");
+
+  const fetchRepos = async () => {
+    const res = await fetch("https://api.github.com/search/repositories?q=Q");
+    if (!res.ok) {
+      throw new Error("Network error");
+    }
+    return res.json();
+  };
+
+  const { data } = useQuery({
+    queryKey: ["repos"],
+    queryFn: fetchRepos,
+  });
+
+  console.log("adora Repo: ", data);
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setValue(e.target.value);
