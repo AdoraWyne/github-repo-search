dc9d41fa83731880737433542413313bb04af298
AdoraWyne <dec_kwynez@hotmail.com>
2026-05-01 02:10:07 +1000

    Create a ApiError class for handling error when making api call

--- src/types/github.ts
+++ src/types/github.ts
@@ -9,3 +9,15 @@ export interface SearchResponse {
   incomplete_results: boolean;
   items: Repo[];
 }
+
+export class ApiError extends Error {
+  status: number;
+  rateLimitReset?: number;
+
+  constructor(message: string, status: number, rateLimitReset?: number) {
+    super(message);
+    this.status = status;
+    this.rateLimitReset = rateLimitReset;
+    this.name = "ApiError";
+  }
+}
