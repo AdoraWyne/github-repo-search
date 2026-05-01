ea044ee585710e188d87cc12730aa75ce6184b1e
AdoraWyne <dec_kwynez@hotmail.com>
2026-05-01 18:02:10 +1000

    Correct typo and file path

--- src/test/setup.ts
+++ src/test/setup.ts
@@ -1,5 +1,5 @@
 import { beforeAll, afterEach, afterAll } from "vitest";
-import { server } from "./src/mocks/node";
+import { server } from "../mocks/node";
 
 beforeAll(() => server.listen());
 afterEach(() => server.resetHandlers()); // reset between tests (so per-test overrides don't leak)
