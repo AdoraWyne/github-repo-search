0c59b232e3201feb2a52d9662549a53aadbcfe38
AdoraWyne <dec_kwynez@hotmail.com>
2026-05-03 22:05:48 +1000

    Update SearchInput component to get q from user input or URL param

--- src/components/SearchInput.tsx
+++ src/components/SearchInput.tsx
@@ -1,35 +1,24 @@
 import { useState } from "react";
-import { useQuery } from "@tanstack/react-query";
+import { useUrlSearchState } from "../hooks/useUrlSearchState";
 
 const SearchInput: React.FC = () => {
-  const [value, setValue] = useState<string>("");
-
-  const fetchRepos = async () => {
-    const res = await fetch("https://api.github.com/search/repositories?q=Q");
-    if (!res.ok) {
-      throw new Error("Network error");
-    }
-    return res.json();
-  };
-
-  const { data } = useQuery({
-    queryKey: ["repos"],
-    queryFn: fetchRepos,
-  });
-
-  console.log("adora Repo: ", data);
+  const { q, setQueryAndResetPage } = useUrlSearchState();
+  const [value, setValue] = useState<string>(q);
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setValue(e.target.value);
   };
 
-  const handleSubmit = (e: React.SubmitEvent<HTMLFormElement>) => {
+  const handleSubmit = (e: React.SyntheticEvent<HTMLFormElement>) => {
     e.preventDefault();
-    alert("query submitted: " + value.trim());
+    setQueryAndResetPage(value.trim());
   };
 
   return (
     <form onSubmit={handleSubmit}>
+      <label htmlFor="search-query" className="sr-only">
+        Search repositories
+      </label>
       <input
         className="border border-gray-300 rounded-sm p-2 w-full bg-pink-50 my-4"
         id="search-query"
@@ -38,10 +27,9 @@ const SearchInput: React.FC = () => {
         type="text"
         value={value}
       />
-      <br />
       <button
         className="border border-gray-300 rounded-sm p-2"
-        disabled={!value}
+        disabled={!value.trim()}
         type="submit"
       >
         Submit
