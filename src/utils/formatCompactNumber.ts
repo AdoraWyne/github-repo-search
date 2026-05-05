0a0ef06383ee3c45b6c2305586d3d1faa7c7f47e
AdoraWyne <dec_kwynez@hotmail.com>
2026-05-06 01:08:48 +1000

    Format Compact Number function
    - rewrite with Intl.NumberFormat to convert the stars count
    - add tests to see the expected behaviour, it's a bit unnecessary, feels like testing the API function but it's good to read what to render

--- src/utils/formatCompactNumber.ts
+++ src/utils/formatCompactNumber.ts
@@ -1,60 +1,7 @@
-// Below 999 -> 999
-
-// 1000 -> 1k
-// 1265 -> 1.2k
-// 10000 -> 10k
-// 18765 -> 18.7k
-// 100000 -> 100k
-// 244772 -> 244k
-
-// 1,000,000 -> 1m
-// 1,673,893 -> 1.6m
-
-// 1,000,000,000 -> 1b
-// 2,397,397,777 -> 2.3b
-
 export function formatCompactNumber(stars: number): string {
-  if (stars <= 999) return stars.toString();
-
-  if (stars >= 1_000 && stars <= 999_999) {
-    if (Number.isInteger(stars / 1_000)) {
-      return `${stars / 1_000}k`;
-    } else {
-      return (stars / 1_000)
-        .toString()
-        .replace(/(^\d+)(.)(\d)(\d+)/, "$1$2$3k");
-    }
-  }
-
-  if (stars >= 1_000_000 && stars <= 999_999_999) {
-    if (Number.isInteger(stars / 1_000_000)) {
-      return `${stars / 1_000_000}m`;
-    } else {
-      return (stars / 1_000_000)
-        .toString()
-        .replace(/(^\d+)(.)(\d)(\d+)/, "$1$2$3m");
-    }
-  }
-
-  if (stars >= 1_000_000_000 && stars <= 999_999_999_999) {
-    if (Number.isInteger(stars / 1_000_000_000)) {
-      return `${stars / 1_000_000_000}b`;
-    } else {
-      return (stars / 1_000_000_000)
-        .toString()
-        .replace(/(^\d+)(.)(\d)(\d+)/, "$1$2$3b");
-    }
-  }
-
-  if (stars >= 1_000_000_000_000 && stars <= 999_999_999_999_999) {
-    if (Number.isInteger(stars / 1_000_000_000_000)) {
-      return `${stars / 1_000_000_000_000}t`;
-    } else {
-      return (stars / 1_000_000_000_000)
-        .toString()
-        .replace(/(^\d+)(.)(\d)(\d+)/, "$1$2$3t");
-    }
-  }
-
-  return "Infinity";
+  const formatter = new Intl.NumberFormat("en", {
+    notation: "compact",
+    maximumFractionDigits: 1,
+  });
+  return formatter.format(stars);
 }
