b00af820eeb2fd8729f2c7e3d5cd87dbcade2401
AdoraWyne <dec_kwynez@hotmail.com>
2026-05-05 19:22:09 +1000

    Hardcoded the datain resultList to build the RepoCard component first

---
+++ src/components/RepoCard.tsx
@@ -0,0 +1,25 @@
+import type { Owner } from "../types/github";
+
+export interface RepoCardProps {
+  id: number;
+  full_name: string;
+  description: string | null;
+  owner?: Owner;
+  html_url: string;
+  stargazers_count: number;
+  language: string | null;
+  updated_at: string;
+}
+
+export const RepoCard = ({
+  id,
+  full_name,
+  description,
+  owner,
+  html_url,
+  stargazers_count,
+  language,
+  updated_at,
+}: RepoCardProps) => {
+  return <></>;
+};
--- src/components/ResultList.tsx
+++ src/components/ResultList.tsx
@@ -1,5 +1,10 @@
 import { useRepoSearch } from "../hooks/useRepoSearch";
 import { useUrlSearchState } from "../hooks/useUrlSearchState";
+import { RepoCard } from "./RepoCard";
+
+// testing ---
+import type { RepoCardProps } from "./RepoCard";
+// ---
 
 const ResultList: React.FC = () => {
   const { q, page, per_page, sort } = useUrlSearchState();
@@ -12,18 +17,47 @@ const ResultList: React.FC = () => {
   if (data && data.items.length === 0) {
     return <p>No repositories found for "{q}"</p>;
   }
+  console.log("adora data: ", data);
+
+  const testingData: RepoCardProps = {
+    id: 10270250,
+    full_name: "facebook/react",
+    description: "The library for web and native user interfaces.",
+    owner: {
+      avatar_url: "https://avatars.githubusercontent.com/u/69631?v=4",
+      url: "https://api.github.com/users/facebook",
+    },
+    html_url: "https://github.com/facebook/react",
+    updated_at: "2026-04-30T16:42:54Z",
+    stargazers_count: 244772,
+    language: "JavaScript",
+  };
 
   return (
-    <ul>
-      {data?.items.map(({ id, full_name, description }) => {
-        return (
-          <li key={id} className="my-4">
-            <p>{full_name}</p>
-            <p>{description}</p>
-          </li>
-        );
-      })}
-    </ul>
+    <>
+      <ul>
+        {data?.items.map(({ id, full_name, description }) => {
+          return (
+            <li key={id} className="my-4">
+              <p>{full_name}</p>
+              <p>{description}</p>
+            </li>
+          );
+        })}
+      </ul>
+
+      <hr />
+      <RepoCard
+        id={testingData.id}
+        full_name={testingData.full_name}
+        description={testingData.description}
+        owner={testingData.owner}
+        html_url={testingData.html_url}
+        stargazers_count={testingData.stargazers_count}
+        language={testingData.language}
+        updated_at={testingData.updated_at}
+      />
+    </>
   );
 };
 
