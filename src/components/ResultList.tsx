8dd461085b0d66df70bd1490de8efb9265d48c20
AdoraWyne <dec_kwynez@hotmail.com>
2026-07-10 02:16:01 +1000

    Render Pagination component on ResultList. Conditionally narrow down data condition so data will not be undefined.

--- src/components/ResultList.tsx
+++ src/components/ResultList.tsx
@@ -3,24 +3,37 @@ import { useRepoSearch } from "../hooks/useRepoSearch";
 import { useUrlSearchState } from "../hooks/useUrlSearchState";
 import { RepoCard } from "./RepoCard";
 import { relativeTime } from "../utils/relativeTime";
+import Pagination from "./Pagination";
+
+const GITHUB_RESULT_CAP = 1_000;
 
 const ResultList: React.FC = () => {
   const [dateNow] = useState(() => Date.now());
-  const { q, page, per_page, sort } = useUrlSearchState();
+  const { q, page, per_page, sort, setPage } = useUrlSearchState();
   const { data } = useRepoSearch({ q, page, per_page, sort });
 
   if (!q.trim()) {
     return <p>What do you want to search?</p>;
   }
 
+  if (!data) {
+    return <p>Loading…</p>;
+  }
+
   if (data && data.items.length === 0) {
     return <p>No repositories found for "{q}"</p>;
   }
 
+  const maxPage = Math.min(
+    Math.ceil(data.total_count / per_page),
+    Math.ceil(GITHUB_RESULT_CAP / per_page),
+  );
+
   return (
     <>
+      <p>About {data.total_count} results.</p>
       <ul>
-        {data?.items.map(
+        {data.items.map(
           ({
             id,
             full_name,
@@ -47,6 +60,13 @@ const ResultList: React.FC = () => {
           },
         )}
       </ul>
+      {data.total_count > per_page && (
+        <Pagination
+          currentPage={page}
+          maxPage={maxPage}
+          onPageChange={setPage}
+        />
+      )}
     </>
   );
 };
