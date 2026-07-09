4f23f2beb3065a1a247b18249f985659fec5c8de
AdoraWyne <dec_kwynez@hotmail.com>
2026-07-10 02:16:40 +1000

    Update the SearchPage test.
    I used server.use test case instead of the MSW handlers to keep the test case close to the test.

--- src/routes/SearchPage.test.tsx
+++ src/routes/SearchPage.test.tsx
@@ -65,4 +65,97 @@ describe("SearchPage", () => {
       screen.getByText(/the library for web and native user interfaces/i),
     ).toBeInTheDocument();
   });
+
+  it("fetches and displays page 2 results when clicking the Page 2 button", async () => {
+    server.use(
+      http.get("https://api.github.com/search/repositories", ({ request }) => {
+        const page = new URL(request.url).searchParams.get("page");
+        if (page === "2") {
+          return HttpResponse.json({
+            total_count: 11,
+            incomplete_results: false,
+            items: [
+              {
+                id: 11,
+                full_name: "facebook/react/11",
+                description: "lorem ipsum 11.",
+              },
+            ],
+          });
+        }
+
+        return HttpResponse.json({
+          total_count: 11,
+          incomplete_results: false,
+          items: [
+            {
+              id: 1,
+              full_name: "facebook/react/1",
+              description: "lorem ipsum 1.",
+            },
+            {
+              id: 2,
+              full_name: "facebook/react/2",
+              description: "lorem ipsum 2.",
+            },
+            {
+              id: 3,
+              full_name: "facebook/react/3",
+              description: "lorem ipsum 3.",
+            },
+            {
+              id: 4,
+              full_name: "facebook/react/4",
+              description: "lorem ipsum 4.",
+            },
+            {
+              id: 5,
+              full_name: "facebook/react/5",
+              description: "lorem ipsum 5.",
+            },
+            {
+              id: 6,
+              full_name: "facebook/react/6",
+              description: "lorem ipsum 6.",
+            },
+            {
+              id: 7,
+              full_name: "facebook/react/7",
+              description: "lorem ipsum 7.",
+            },
+            {
+              id: 8,
+              full_name: "facebook/react/8",
+              description: "lorem ipsum 8.",
+            },
+            {
+              id: 9,
+              full_name: "facebook/react/9",
+              description: "lorem ipsum 9.",
+            },
+            {
+              id: 10,
+              full_name: "facebook/react/10",
+              description: "lorem ipsum 10.",
+            },
+          ],
+        });
+      }),
+    );
+
+    const user = userEvent.setup();
+    renderPage();
+
+    await user.type(screen.getByRole("textbox"), "react");
+    await user.click(screen.getByRole("button", { name: /submit/i }));
+
+    const item = await screen.findByText("facebook/react/1");
+    expect(item).toBeInTheDocument();
+
+    const pageButton = await screen.findByRole("button", { name: "Page 2" });
+    await user.click(pageButton);
+
+    const item2 = await screen.findByText("facebook/react/11");
+    expect(item2).toBeInTheDocument();
+  });
 });
