0c4b3e57a2354fd3801080bac70c7f863f01a9cc
AdoraWyne <dec_kwynez@hotmail.com>
2026-07-21 00:22:02 +1000

    RepoCard component and add docs for <article>
    use <article> instead of <div> to represent this is a self-contained piece of content

--- src/components/RepoCard.tsx
+++ src/components/RepoCard.tsx
@@ -23,44 +23,42 @@ export const RepoCard = ({
   const starsLabel = `${formatCompactNumber(stargazers_count)} ${stargazers_count === 1 ? "star" : "stars"}`;
 
   return (
-    <>
-      <div className="border border-solid border-pink-300 rounded-md p-4">
-        {/* Header */}
-        <div className="flex items-center">
-          <img src={owner?.avatar_url} className="w-7 h-7 inline mr-2" />
-          <span>
-            <a
-              href={html_url}
-              target="_blank"
-              className="text-pink-500 text-lg font-semibold"
-            >
-              {full_name}
-            </a>
-          </span>
-        </div>
-
-        {/* Description */}
-        {description && (
-          <div className="mt-2">
-            <span>{description}</span>
-          </div>
-        )}
-
-        {/* Footer */}
-        <div className="mt-2">
-          <ul className="flex text-gray-500 text-sm items-center flex-wrap">
-            {language && (
-              <>
-                <li>{language}</li>
-                <span className="mx-2">.</span>
-              </>
-            )}
-            <li>{starsLabel}</li>
-            <span className="mx-2">.</span>
-            <li>{updatedLabel}</li>
-          </ul>
-        </div>
+    <article className="border border-solid border-pink-300 rounded-md p-4">
+      {/* Header */}
+      <div className="flex items-center">
+        <img src={owner?.avatar_url} className="w-7 h-7 inline mr-2" />
+        <span>
+          <a
+            href={html_url}
+            target="_blank"
+            className="text-pink-500 text-lg font-semibold"
+          >
+            {full_name}
+          </a>
+        </span>
       </div>
-    </>
+
+      {/* Description */}
+      {description && (
+        <div className="mt-2">
+          <span>{description}</span>
+        </div>
+      )}
+
+      {/* Footer */}
+      <div className="mt-2">
+        <ul className="flex text-gray-500 text-sm items-center flex-wrap">
+          {language && (
+            <>
+              <li>{language}</li>
+              <span className="mx-2">.</span>
+            </>
+          )}
+          <li>{starsLabel}</li>
+          <span className="mx-2">.</span>
+          <li>{updatedLabel}</li>
+        </ul>
+      </div>
+    </article>
   );
 };
